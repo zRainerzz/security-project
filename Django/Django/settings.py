795455599ea350c96e01eb470974b0ef35c1e2c6
zRainerzz <ghaithkhedhri3@gmail.com>
2025-01-20 15:27:49 +0100

    sq

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'login.apps.LoginConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Django/login/apps.py
+++ Django/login/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PagesConfig(AppConfig):
+class LoginConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pages'
+    name = 'login'
