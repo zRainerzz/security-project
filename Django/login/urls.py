889af9ab869bee3e4bdc27d2f0f5ee387a96ea5d
zRainerzz <ghaithkhedhri3@gmail.com>
2025-01-23 14:00:01 +0100

    :V

--- Django/login/urls.py
+++ Django/login/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='home_page'),  # This sets the login view as the root URL
+    path('', views.login, name='login_page'),  # This sets the login view as the root URL
 
 ]
