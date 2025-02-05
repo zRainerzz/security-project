7909d5c8af8a19afdc8dc1f9555cbded7e87fb56
zRainerzz <ghaithkhedhri3@gmail.com>
2025-02-05 13:42:35 +0100

    ss

---
+++ Django/home/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Data
+
+class DataForm(forms.ModelForm):
+    class Meta:
+        model = Data
+        fields = ['text']
--- Django/home/views.py
+++ Django/home/views.py
@@ -1,11 +1,20 @@
 # filepath: /c:/Users/lenovo/Desktop/security-project/Django/login/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import DataForm
 # Create your views here.
 
 def home(request):
-    editors = {'name1':'amen allah naamen', 'name2':'hazem saidani','name3': 'ghaith khedhri'}
+    editors = {'name1': 'amen allah naamen', 'name2': 'hazem saidani', 'name3': 'ghaith khedhri'}
     return render(request, 'home/index.html', editors)
 
-
-
+# New view for handling the form submission
+def index(request):
+    if request.method == 'POST':
+        form = DataForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')  # Redirect to the same page after saving
+    else:
+        form = DataForm()
+    return render(request, 'home/index.html', {'form': form})
